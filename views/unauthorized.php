84e7872f4fd6a7130b5241aeeed80e7cfb645fbe
diegogranados143@gmail.com <diego@codeando.edu.pe>
2023-10-01 09:27:43 -0400

    feat: unauthorized page

---
+++ views/unauthorized.php
@@ -0,0 +1,9 @@
+<div>
+    <h1 class="mb-2">Error!</h1>
+    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+        <div class="bg-white rounded-md border border-gray-100 p-6">
+            <h2>Lo sentimos</h2>
+            <p><?php echo $message ?></p>
+        </div>
+    </div>
+</div>
