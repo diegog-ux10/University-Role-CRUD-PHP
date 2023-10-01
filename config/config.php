3d24c64d1ea8218f5b3d81cffc35e427ee6bb3e8
diegogranados143@gmail.com <diego@codeando.edu.pe>
2023-10-01 09:26:47 -0400

    feat: neccesary config files

--- config/config.php
+++ config/config.php
@@ -2,7 +2,7 @@
 
 $config = [
     "db" => [
-        "db_dsb" => "mysql:host=localhost;port=3306;dbname=university",
+        "db_dsb" => "mysql:host=localhost;port=3306;dbname=universidad",
         "db_user" => "root",
         "db_password" => "",
     ]
