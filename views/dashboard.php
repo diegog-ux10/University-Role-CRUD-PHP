57befbd296ced3e328b5b3d77e518c6223c646b2
diegogranados143@gmail.com <diego@codeando.edu.pe>
2023-10-01 09:27:16 -0400

    feat: dashboard page

---
+++ views/dashboard.php
@@ -0,0 +1,9 @@
+<div>
+  <h1 class="mb-2">Dashboard</h1>
+  <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+    <div class="bg-white rounded-md border border-gray-100 p-6">
+        <h2>Bienvenido</h2>
+        <p>Selecciona la acción que quieras realizar en las pestañas del menú a la izquierda</p>
+    </div>
+  </div>
+</div>
